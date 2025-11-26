c85a485b5574928774478f88bef4de8a5eeef3bf
tiru94397 <tiruvenkatesh123@gmail.com>
2025-11-26 16:01:32 +0530

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDQXE5ICednf0AHESEViOuzdvwNS5oS-rk",
+  authDomain: "realmind-1.firebaseapp.com",
+  projectId: "realmind-1",
+  storageBucket: "realmind-1.firebasestorage.app",
+  messagingSenderId: "424947466240",
+  appId: "1:424947466240:web:148379b2568d923f2498e5",
+  measurementId: "G-4RDPDTDJYP"
+};
